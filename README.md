441ae29b7ab68dda8a059a61eda3cd2d7928b1de
book81able <ecobb81@gmail.com>
2020-04-07 19:40:16 -0400

    Database update (added proper forgien keys)

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # VideoGameDatabase
 Final Project for CS 3400: Database Design
+
+# Instructions for Running Project
+
+SQL:
+To run the application, have a mysql server running on your machine. Run the database.sql script to initilize the database.
+
+CLIENT:
+Specfications: Python 3 with Tkinter and pymysql. 
+
+Apple:
+The application works on MacOS Catalina, however due to problems with tkinter, it will crash a computer running mojave. 
+
+Windows:
+
--- database.sql
+++ database.sql
@@ -5,7 +5,7 @@ USE `gamePlay`;
 CREATE TABLE IF NOT EXISTS `gamePlay`.`player` (
 	`playerID` INT NOT NULL,
     `name` VARCHAR(45) NOT NULL,
-    `gender` VARCHAR(255) NOT NULL,
+    `gender` VARCHAR(45) NOT NULL,
     CONSTRAINT player_pk PRIMARY KEY (`playerID`))
 ;
 
@@ -36,6 +36,18 @@ CREATE TABLE IF NOT EXISTS `gamePlay`.`platform` (
     CONSTRAINT platform_pk PRIMARY KEY (`platformID`))
 ;
 
+CREATE TABLE IF NOT EXISTS `gamePlay`.`gameplayGenre` (
+	`gGenreID` INT NOT NULL,
+	`gGenreTitle` VARCHAR(45) NOT NULL,
+	CONSTRAINT game_genre_pk PRIMARY KEY (`gGenreID`))
+;
+
+CREATE TABLE IF NOT EXISTS `gamePlay`.`aestheticGenre` (
+	`aGenreID` INT NOT NULL,
+	`aGenreTitle` VARCHAR(45) NOT NULL,
+	CONSTRAINT game_genre_pk PRIMARY KEY (`aGenreID`))
+;
+
 CREATE TABLE IF NOT EXISTS `gamePlay`.`game` (
 	`gameID` INT NOT NULL,
     `title` VARCHAR(45) NOT NULL,
@@ -43,8 +55,8 @@ CREATE TABLE IF NOT EXISTS `gamePlay`.`game` (
 	`developerID` INT NOT NULL,
 	`publisherID` INT NOT NULL,
 	`ageRating` INT NOT NULL,
-    `gameplayGenre` VARCHAR(45) NOT NULL,
-	`aestheticGenre` VARCHAR(45) NOT NULL,
+    `gameplayGenre` INT NOT NULL,
+	`aestheticGenre` INT NOT NULL,
 	`localPlayer` VARCHAR(45),
 	`onlinePlayer` VARCHAR(45),
 	`has_multiplayer` VARCHAR(45) NOT NULL,
@@ -56,25 +68,73 @@ CREATE TABLE IF NOT EXISTS `gamePlay`.`game` (
         ON UPDATE CASCADE ON DELETE CASCADE,
 	CONSTRAINT game_fk2 FOREIGN KEY (`publisherID`) 
 		references publisher(`publisherID`) 
-        ON UPDATE CASCADE ON DELETE CASCADE)
+        ON UPDATE CASCADE ON DELETE CASCADE),
+	CONSTRAINT game_fk3 FOREIGN KEY (`gameplayGenre`) 
+		references gameplayGenre(`gGenreID`) 
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT game_fk4 FOREIGN KEY (`aestheticGenre`) 
+		references aestheticGenre(`aGenreID`) 
+        ON UPDATE CASCADE ON DELETE CASCADE,
 ;
 
 CREATE TABLE IF NOT EXISTS `gamePlay`.`player_game` (
 	`playerID` INT NOT NULL,
 	`gameID` INT NOT NULL,
 	`playtime` INT,
-    CONSTRAINT playergame_pk PRIMARY KEY (`playerID`, `gameID`))
+    CONSTRAINT playergame_pk PRIMARY KEY (`playerID`, `gameID`)),
+	CONSTRAINT game_player_fk1 FOREIGN KEY (`playerID`) 
+		references player(`playerID`) 
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT game_player_fk2 FOREIGN KEY (`gameID`) 
+		references game(`gameID`) 
+        ON UPDATE CASCADE ON DELETE CASCADE
 ;
 
 CREATE TABLE IF NOT EXISTS `gamePlay`.`reviewer_game` (
 	`reviewerID` INT NOT NULL,
 	`gameID` INT NOT NULL,
 	`reviewScore` INT NOT NULL,
-    CONSTRAINT reviewergame_pk PRIMARY KEY (`reviewerID`, `gameID`))
+    CONSTRAINT reviewergame_pk PRIMARY KEY (`reviewerID`, `gameID`)),
+	CONSTRAINT game_review_fk1 FOREIGN KEY (`reviewerID`) 
+			references reviewer(`reviewerID`) 
+	        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT game_review_fk2 FOREIGN KEY (`gameID`) 
+		references game(`gameID`) 
+        ON UPDATE CASCADE ON DELETE CASCADE
 ;
 
 CREATE TABLE IF NOT EXISTS `gamePlay`.`platform_game` (
 	`platformID` INT NOT NULL,
 	`gameID` INT NOT NULL,
-    CONSTRAINT platformgame_pk PRIMARY KEY (`platformID`, `gameID`))
+    CONSTRAINT platformgame_pk PRIMARY KEY (`platformID`, `gameID`)),
+	CONSTRAINT game_platform_fk1 FOREIGN KEY (`platformID`) 
+		references platform(`platformID`) 
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT game_platform_fk2 FOREIGN KEY (`gameID`) 
+		references game(`gameID`) 
+        ON UPDATE CASCADE ON DELETE CASCADE
+;
+
+CREATE TABLE IF NOT EXISTS `gamePlay`.`gGenre_game` (
+	`gGenreID` INT NOT NULL,
+	`gameID` INT NOT NULL,
+    CONSTRAINT ggenre_game_pk PRIMARY KEY (`gGenreID`, `gameID`)),
+	CONSTRAINT game_gameplay_fk1 FOREIGN KEY (`gGenreID`) 
+		references gameplayGenre(`gGenreID`) 
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT game_gameplay_fk2 FOREIGN KEY (`gameID`) 
+		references game(`gameID`) 
+        ON UPDATE CASCADE ON DELETE CASCADE
+;
+
+CREATE TABLE IF NOT EXISTS `gamePlay`.`aGenre_game` (
+	`aGenreID` INT NOT NULL,
+	`gameID` INT NOT NULL,
+    CONSTRAINT agenre_game_pk PRIMARY KEY (`aGenreID`, `gameID`)),
+	CONSTRAINT game_aesthics_fk1 FOREIGN KEY (`aGenreID`) 
+		references aestheticGenre(`aGerneID`) 
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT game_aesthics_fk2 FOREIGN KEY (`gameID`) 
+		references game(`gameID`) 
+        ON UPDATE CASCADE ON DELETE CASCADE
 ;
